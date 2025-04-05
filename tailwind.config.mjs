8e7f375bc8eb105c359c4bc3d3fd58a5a2cb61ad
Miller Mosquera <millerpalacios16@gmail.com>
2025-04-04 23:32:36 -0500

    Mejora el diseño del encabezado y la página "Acerca de" con nuevos estilos y un tooltip; se agregó animaciones en la configuración de Tailwind.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-      extend: {},
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      animation: {
+        'ping': 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite',
+      },
+      keyframes: {
+        ping: {
+          '0%': { transform: 'scale(0.95)', opacity: '0.8' },
+          '70%': { transform: 'scale(1.3)', opacity: '0' },
+          '100%': { transform: 'scale(0.95)', opacity: '0' },
+        }
+      }
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
