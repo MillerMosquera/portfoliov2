8082368020598552934f1445f3e59d8d97f6c47b
Miller Mosquera <millerpalacios16@gmail.com>
2025-04-06 22:07:52 -0500

    Actualiza la página "Acerca de" con nueva descripción y estilos; implementa un diseño de girdbento para la sección de proyectos y mejora la estructura de datos de proyectos y habilidades.

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,7 +2,6 @@ import Home from '@/assets/header/home.svg';
 import Projects from '@/assets/header/projects.svg';
 import User from '@/assets/header/user.svg';
 
-
 import Linkedin from "@/assets/socials/linkedin.svg";
 import Github from "@/assets/socials/Github.svg";
 import Mail from "@/assets/socials/mail.svg";
@@ -46,7 +45,7 @@ export const workExperience = [
         company: "XXXXXX",
         position: "XXXXXX",
         duration: "Año - Presente",
-        description:[
+        description: [
             "XXXXXXXXXXXX",
             "XXXXXXXXXXX",
         ],
@@ -77,13 +76,16 @@ export const studies = [
 ];
 
 export const skills = [
+    { image: "", text: "Java" },
+    { image: "", text: "Spring boot" },
     { image: "", text: "JavaScript" },
-    { text: "React" },
-    { text: "Node.js" },
-    { text: "Tailwind CSS" },
-    { text: "Figma" },
-    { text: "Photoshop" },
+    { image: "", text: "HTML" },
+    { image: "", text: "CSS" },
+    { image: "", text: "Node.js" },
+    { image: "", text: "PostgreSQL" },
+    { image: "", text: "Git" },
 ];
+
 export const hobbies = [
     { text: "Guitarra" },
     { text: "Gym" },
@@ -93,48 +95,39 @@ export const hobbies = [
 
 export const projects = [
     {
-      id: 1,
-      title: 'This is bento grid in vercel blog',
-      author: 'Miller Mosquera',
-      date: '12.10.1999',
-      image: 'https://picsum.photos/seed/1/600/400',
-      avatar: 'https://i.pravatar.cc/150?img=1',
-      link: '/',
+        id: 1,
+        title: 'This is bento grid',
+        author: 'Miller Mosquera',
+        date: '12.10.1999',
+        image: 'https://picsum.photos/seed/1/600/400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        link: '/',
     },
     {
-      id: 2,
-      title: 'Exploring the Future of Web Development',
-      author: 'Miller Mosquera',
-      date: '05.07.2023',
-      image: 'https://picsum.photos/seed/2/600/400',
-      avatar: 'https://i.pravatar.cc/150?img=2',
-      link: '/',
+        id: 2,
+        title: 'Exploring the Future of Web Development',
+        author: 'Miller Mosquera',
+        date: '05.07.2023',
+        image: 'https://picsum.photos/seed/2/600/400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        link: '/',
     },
     {
-      id: 3,
-      title: 'How to Build Scalable Applications',
-      author: 'Miller Mosquera',
-      date: '18.03.2021',
-      image: 'https://picsum.photos/seed/3/600/400',
-      avatar: 'https://i.pravatar.cc/150?img=3',
-      link: '/',
+        id: 3,
+        title: 'How to Build Scalable Applications',
+        author: 'Miller Mosquera',
+        date: '18.03.2021',
+        image: 'https://picsum.photos/seed/3/600/400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        link: '/',
     },
     {
-      id: 4,
-      title: 'Mastering React with Next.js',
-      author: 'Miller Mosquera',
-      date: '24.12.2022',
-      image: 'https://picsum.photos/seed/4/600/400',
-      avatar: 'https://i.pravatar.cc/150?img=4',
-      link: '/',
+        id: 4,
+        title: 'Mastering React with Next.js',
+        author: 'Miller Mosquera',
+        date: '24.12.2022',
+        image: 'https://picsum.photos/seed/3/600/400',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        link: '/',
     },
-    {
-      id: 5,
-      title: 'Understanding TypeScript in 2024',
-      author: 'Miller Mosquera',
-      date: '02.11.2024',
-      image: 'https://picsum.photos/seed/5/600/400',
-      avatar: 'https://i.pravatar.cc/150?img=5',
-      link: '/',
-    },
-  ]
+]
